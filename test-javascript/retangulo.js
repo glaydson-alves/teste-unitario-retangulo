7dbc4d8baa9c251b102a241b0a6a79ee4ed84c26
glaydson-alves <glaydson.alves36@gmail.com>
2023-10-22 18:47:48 -0300

    iniciado teste js

---
+++ test-javascript/retangulo.js
@@ -0,0 +1,27 @@
+
+const Retangulo = {
+    largura: 0,
+    altura: 0,
+  
+    definirDimensoes: function(largura, altura) {
+      if (largura > 0 && altura > 0) {
+        this.largura = largura
+        this.altura = altura
+        return true
+      } else {
+        return false
+      }
+    },
+  
+    calcularArea: function() {
+      return this.largura * this.altura
+    },
+  
+    calcularPerimetro: function() {
+      return 2 * (this.largura + this.altura)
+    }
+}
+  
+
+module.exports = Retangulo
+  
